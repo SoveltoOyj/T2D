986c59bdc8d899a77a5467c6a5443f106ec03d67
Ahti <ahti.haukilehto@palkkaus.fi>
2016-10-14 13:22:03 +0300

    demo git:N käytöstä

--- src/T2D.Model/InventoryApi/GetRelations.cs
+++ src/T2D.Model/InventoryApi/GetRelations.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace T2D.Model.InventoryApi
 {
-
+	// request-luokan kantaluokka (demo git:n käytöstä, poista tämä turha kommentti)
 	public abstract class GetAttributeRelationsBaseRequest
 	{
 		public string Session { get; set; }
