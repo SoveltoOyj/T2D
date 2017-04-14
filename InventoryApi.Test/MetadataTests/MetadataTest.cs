58302de72d44961be6f8d004f83cb23c3015be95
Ahti <ahti.haukilehto@palkkaus.fi>
2017-04-14 20:00:28 +0300

    metadatacontroller enchangements

--- InventoryApi.Test/MetadataTests/MetadataTest.cs
+++ InventoryApi.Test/MetadataTests/MetadataTest.cs
@@ -9,9 +9,9 @@ using Xunit;
 
 namespace InventoryApi.Test
 {
-	public class Core1Test : InventoryApiBase
+	public class MetadataTest : InventoryApiBase
 	{
-		private string _url = "api/metadata/version";
+		private string _url = "api/metadata";
 
 		[Fact]
 		public async void GetApiVersion_ShouldReturnData()
@@ -24,5 +24,21 @@ namespace InventoryApi.Test
 			Assert.NotEmpty(result);
 			Assert.Equal<string>("1.0.0.0", result);
 		}
+
+
+		[Fact]
+		public async void GetEnumNames_ShouldReturnData()
+		{
+			var response = await _client.GetAsync($"{_url}/EnumNames");
+//			var result = await response.Content.ReadAsStringAsync();
+			var result = await response.Content.ReadAsJsonAsync<List<string>>();
+
+			response.EnsureSuccessStatusCode();
+			Assert.NotNull(result);
+			Assert.NotEmpty(result);
+			Assert.True(result.Count > 5);
+			Assert.True(result.Contains("Role"));
+		}
+
 	}
 }
--- src/InventoryApi/Controllers/MetadataControllers/MetadataController.cs
+++ src/InventoryApi/Controllers/MetadataControllers/MetadataController.cs
@@ -5,12 +5,23 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using T2D.InventoryBL.Mappers;
 using InventoryApi.Controllers.BaseControllers;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+using System.Reflection;
 
 namespace InventoryApi.Controllers.MetadataControllers
 {
-	[Route("api/metadata/[controller]/[Action]")]
-	public class VersionController : ApiBaseController
+	[Route("api/[controller]/[Action]")]
+	public class MetadataController : ApiBaseController
 	{
+
+		private readonly IHostingEnvironment _env;
+
+		public MetadataController(IHostingEnvironment env)
+		{
+			_env = env;
+		}
+
 		/// <summary>
 		/// Get Inventory API version number.
 		/// </summary>
@@ -18,13 +29,13 @@ namespace InventoryApi.Controllers.MetadataControllers
 		/// <response code="200">Returns version number.</response>
 		[HttpGet(), ActionName("ApiVersion")]
 		[Produces(typeof(string))]
-		public IActionResult Get()
+		public IActionResult ApiVersion()
 		{
 			return Ok("1.0.0.0");
 		}
 
 		/// <summary>
-		/// Get API compatibility to specific version.
+		/// Get API compatibility to specific version. Not yet implemented!
 		/// </summary>
 		/// <param name="version">Version to which compatibility is compared to.</param>
 		/// <returns>List on functions this Inventory compatible to.</returns>
@@ -32,7 +43,7 @@ namespace InventoryApi.Controllers.MetadataControllers
 		/// <response code="400">Version is not correct.</response>
 		[HttpGet(), ActionName("ApiCompatibility")]
 		[Produces(typeof(List<string>))]
-		public IActionResult GetApiCompatibility(string version)
+		public IActionResult ApiCompatibility(string version)
 		{
 			if (string.IsNullOrWhiteSpace(version)) return BadRequest("version is empty or null.");
 
@@ -42,6 +53,32 @@ namespace InventoryApi.Controllers.MetadataControllers
 			};
 			return Ok(ret);
 		}
+
+
+		/// <summary>
+		/// Get all enum names.
+		/// </summary>
+		/// <returns>List on enum names.</returns>
+		/// <response code="200">Returns enum names.</response>
+		[HttpGet(), ActionName("EnumNames")]
+		[Produces(typeof(List<string>))]
+		public IActionResult EnumNames()
+		{
+			List<string> ret = new List<string>
+			{
+				"AttributeType",
+				"FunctionalStatus",
+				"LocationType",
+				"Relation",
+				"Right",
+				"Role",
+				"ServiceAndActivityState",
+				"ThingStatus",
+			};
+			return Ok(ret);
+		}
+
+
 
 
 	}
